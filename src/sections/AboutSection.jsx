8b6d4a600e9fe65b7a9ac8414d6de6eb8beca5b5
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-05-17 13:26:19 +0100

    chore: add placeholder components for About, Projects, Certificates, Contact, and Footer section

---
+++ src/sections/AboutSection.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const AboutSection = () => {
+
+    return (
+        <div id="about">
+            <h2>About Me</h2>
+        </div>
+    );
+};
+
+export default AboutSection;
