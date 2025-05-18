f0abd9e28e96a6423e8406d2417b725f9d5cdc45
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-05-18 03:40:28 +0100

    Improved Navbar.jsx for mobile visibility and fixed toggle position issue

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import logo from '../assets/icons/logo.png'
+import { Menu } from "lucide-react";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
 
-
-    return (
-      <nav className="bg-[#121212] shadow-md fixed w-full z-10 px-20">
-        <div className="flex items-center justify-between px-4 py-2">
+  return (
+    <>
+      <nav className="bg-[#121212] shadow-md fixed w-full z-10 sm:px-4">
+        <div className="container flex items-center justify-between mx-auto py-1">
           <div>
             <a href="#">
               <img src={logo} alt="Logo" className="h-20" />
             </a>
           </div>
 
-          <ul className="flex space-x-24">
+          <ul className="hidden space-x-16 md:flex items-center">
             <li>
               <a href="#">Home</a>
             </li>
@@ -27,9 +31,48 @@ const NavBar = () => {
               <a href="#contact">Contact</a>
             </li>
           </ul>
+          <button
+            onClick={toggleMenu}
+            className="text-[#FFF] md:hidden sm:flex"
+          >
+            <Menu />
+          </button>
         </div>
       </nav>
-    );
+
+      <div
+        id="mobile-menu"
+        className="flex md:hidden bg-[#121212] shadow-lg fixed w-full"
+      >
+        <div
+          className={`container mx-auto px-4 pt-24 pb-3 space-y-3 flex flex-col ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <span>
+            <a href="#" onClick={closeMenu}>
+              Home
+            </a>
+          </span>
+          <span>
+            <a href="#about" onClick={closeMenu}>
+              About
+            </a>
+          </span>
+          <span>
+            <a href="#project" onClick={closeMenu}>
+              Projects
+            </a>
+          </span>
+          <span>
+            <a href="#contact" onClick={closeMenu}>
+              Contact
+            </a>
+          </span>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
