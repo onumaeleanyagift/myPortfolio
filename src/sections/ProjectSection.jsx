cd36666e7b3b612a2f7fa27e4c8b5531951f9400
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-05-18 03:47:48 +0100

    fix: adjust Project section layout

---
+++ src/sections/ProjectSection.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const ProjectSection = () => {
+    return (
+    <div>
+        <div></div>
+        </div>
+    );
+};
+
+export default ProjectSection;
