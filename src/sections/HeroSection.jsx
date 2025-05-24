4775d567460d719572d6cbf3abb8eab9ab7dc54c
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-05-24 22:30:22 +0100

    fix: enable buttons in HeroSection to navigate to page sections

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import image from '../assets/images/Onuma.jpg';
 import { ArrowRight } from "lucide-react";
 import { ChevronDown } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
-  const handleScroll = () => {
-    document.getElementById('about')?.scrollIntoView({ behaviour: 'smooth' });
+  const handleScroll = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
   };
+ 
 
     return (
       <div className="bg-[#191919]">
@@ -23,14 +28,14 @@ const HeroSection = () => {
                 I craft responsive websites where technologies meet creativity
               </p>
               <button
-                onClick={() => Navigate("/ProjectSection")}
+                onClick={() => handleScroll("project")}
                 className="inline-flex items-center bg-[#2CCEB9] gap-1 hover:bg-[#14B8A6] hover:border-[#14B8A6] lg:text-lg"
               >
                 View My Work
                 <ArrowRight className="w-5 h-5" />
               </button>
               <button
-                onClick={() => Navigate("/ContactSection")}
+                onClick={() => handleScroll("contact")}
                 className="bg-[#161616] border-[#4B5563] hover:border-[#14B8A6] mx-2 lg:mx-4 lg:text-lg"
               >
                 Contact Me
@@ -44,7 +49,10 @@ const HeroSection = () => {
               />
             </div>
           </div>
-          <div onClick={handleScroll} className="flex justify-center pt-16 pb-4 lg:pt-32 lg:pb-8">
+          <div
+            onClick={() => handleScroll("about")}
+            className="flex justify-center pt-16 pb-4 lg:pt-32 lg:pb-8"
+          >
             <ChevronDown className="w-10 h-10 text-gray-500 animate-bounce cursor-pointer hover:text-[#14B8A6]" />
           </div>
         </div>
